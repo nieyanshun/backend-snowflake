2f68e649e825a87448017ca93e6fa50dbad113c8
nieyanshun <nieyanshun@le.com>
2017-07-24 16:08:06 +0800

    snowflake

---
+++ src/main/java/backend_snowflake_service/backend_snowflake_service/App.java
@@ -0,0 +1,13 @@
+package backend_snowflake_service.backend_snowflake_service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
